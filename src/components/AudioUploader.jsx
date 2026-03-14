f2c637e1b72447814839f2ad7278a722d566173e
jupitters <sampaiobale@gmail.com>
2026-03-14 11:08:53 -0300

    audo uploader component

--- src/components/AudioUploader.jsx
+++ src/components/AudioUploader.jsx
@@ -1,16 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const AudioUploader = () => {
+    const [result, setResult] = useState('');
+
+    const handleTranscriber = async () => {
+        try {
+            const response = fetch("http://localhost:8080/api/audio-transcriber?file=${file}")
+            setResult(response)
+        } catch (error) {
+            alert(error)
+        }
+    }
+
   return (
     <div className="container">
         <h1>Audio to Text Transcriber</h1>
         <div className='file-input'>
             <input type='file' accept='audio/*' />
         </div>
-        <button className='upload-button'>Upload and Transcribe</button>
+        <button className='upload-button' onClick={handleTranscriber}>Upload and Transcribe</button>
         <div className='transcription-result'>
             <h2>Transcription Result</h2>
-            <p></p>
+            <p>{result}</p>
         </div>
     </div>
   )
