3c9727c6f53f8fe096ff9b13aaa433298b0cec5a
jupitters <sampaiobale@gmail.com>
2026-03-14 11:06:03 -0300

    audo uploader component

--- src/components/AudioUploader.jsx
+++ src/components/AudioUploader.jsx
@@ -2,7 +2,13 @@ import React from 'react'
 
 const AudioUploader = () => {
   return (
-    <div>AudioUploader</div>
+    <div className="container">
+        <h1>Audio to Text Transcriber</h1>
+        <div className='file-input'>
+            <input type='file' accept='audio/*' />
+        </div>
+        <button className='upload-button'>Upload and Transcribe</button>
+    </div>
   )
 }
 
